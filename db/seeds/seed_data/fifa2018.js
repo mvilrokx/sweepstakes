1d575ef2a2164c69cae76549a47e076826c71bb3
Mark Vilrokx <mvilrokx@gmail.com>
2018-01-18 16:42:31 -0800

    Added 2018 World Cup Tournament

---
+++ db/seeds/seed_data/fifa2018.js
@@ -0,0 +1,16 @@
+module.exports = {
+  name: 'FIFA 2018',
+  hosts: ['Russia'],
+  starts_at: new Date(2018, 6, 14, 12, 00, 00),
+  ends_at: new Date(2016, 7, 15, 12, 00, 00),
+  groups: {
+    A: ['RUS', 'SAU', 'EGY', 'URY'],
+    B: ['PRT', 'URY', 'MAR', 'IRN'],
+    C: ['FRA', 'AUS', 'PER', 'DNK'],
+    D: ['ARG', 'ISL', 'HRV', 'NGA'],
+    E: ['BRA', 'CHE', 'CRI', 'SRB'],
+    F: ['DEU', 'MEX', 'SWE', 'KOR'],
+    G: ['BEL', 'PAN', 'TUN', 'ENG'],
+    H: ['POL', 'SEN', 'COL', 'JPN']
+  }
+}
